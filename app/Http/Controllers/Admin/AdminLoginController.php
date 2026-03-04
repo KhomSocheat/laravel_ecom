166eb3b3058ce58f232d5086f9be74db1e804069
Khom Socheat <khomsocheat@acledaaib.onmicrosoft.com>
2026-03-04 09:41:44 +0700

    Create Project and setup middleware , create authenticate function then validate in frontend

---
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AdminLoginController extends Controller
+{
+    public function index()
+    {
+        return view('admin.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        if ($validator->passes()) {
+            Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'));
+        } else {
+            return redirect()->route('admin.login')
+                ->withErrors($validator)
+                ->withInput($request->only('email'));
+        }
+    }
+}
